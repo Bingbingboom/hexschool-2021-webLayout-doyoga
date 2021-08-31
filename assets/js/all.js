525a0900c53abb850f50c207a2cfed38527d4f00
Bingbingboom <50125519+Bingbingboom@users.noreply.github.com>
2021-08-31 22:30:10 +0800

    Update 2021-08-31T14:29:41.376Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,148 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  // 首頁 - 精心設計、課程多元 swiper
+  var courseSwiper = new Swiper(".courseSwiper", {
+    slidesPerView: 1.34,
+    spaceBetween: 30
+  }); // 首頁 - 聽聽他們怎麼說 swiper
+
+  var commentSwiper = new Swiper('.commentSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grid: {
+      rows: 3,
+      fill: 'row'
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        grid: {
+          rows: 2
+        }
+      },
+      992: {
+        slidesPerView: 3,
+        grid: {
+          rows: 2
+        }
+      }
+    }
+  }); // 課程介紹頁 - 師資介紹
+
+  var teachersSwiper = new Swiper('.teachersSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grid: {
+      rows: 4,
+      fill: 'row'
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        grid: {
+          rows: 1
+        }
+      },
+      992: {
+        slidesPerView: 3,
+        grid: {
+          rows: 1
+        }
+      }
+    }
+  }); // 瑜珈空間 - 課程推薦 swiper
+
+  var coursesRecommendSwiper = new Swiper(".coursesRecommendSwiper", {
+    slidesPerView: 1.34,
+    spaceBetween: 30,
+    breakpoints: {
+      768: {
+        slidesPerView: 3
+      },
+      992: {
+        slidesPerView: 4.34
+      }
+    }
+  }); // 立即預約選擇方案頁面
+  // 尚未製作點選不同方案改變最終選擇課程名稱（只有初中高階）
+
+  $('.js-schemeCard').click(function (e) {
+    $(this).addClass('active');
+    $(this).parent().siblings().find('.card').removeClass('active');
+    $(this).parent().removeClass('remove');
+    $(this).parent().siblings().addClass('remove'); // 開啟繼續預約按鈕、選擇課程 swiper
+
+    $('.js-contiBtn, .js-chooseLv').removeClass('d-none'); // 立即預約 - 選擇課程等級 swiper
+
+    var courseLevelSwiper = new Swiper('.courseLevelSwiper', {
+      slidesPerView: 1,
+      slidesPerColumn: 3,
+      slidesPerColumnFill: 'row',
+      spaceBetween: 30,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          slidesPerColumn: 1,
+          spaceBetween: 30
+        },
+        992: {
+          slidesPerView: 3,
+          slidesPerColumn: 1,
+          spaceBetween: 20
+        }
+      }
+    }); // 預設選擇基礎課程
+
+    $('.js-firstLvBtn').addClass('active');
+    $('.js-firstLvBtn').parent().siblings().find('button').removeClass('active');
+    $('.js-firstLv').removeClass('d-none');
+    $('.js-firstLv').siblings().addClass('d-none');
+  }); // 立即預約 - 箭頭
+
+  $('.js-first').click(function (e) {
+    e.preventDefault();
+    $('.first').toggleClass('d-none');
+    $('.first').parent().siblings().find('span').addClass('d-none');
+  });
+  $('.js-short').click(function (e) {
+    e.preventDefault();
+    $('.short').toggleClass('d-none');
+    $('.short').parent().siblings().find('span').addClass('d-none');
+  });
+  $('.js-long').click(function (e) {
+    e.preventDefault();
+    $('.long').toggleClass('d-none');
+    $('.long').parent().siblings().find('span').addClass('d-none');
+  });
+  $('.js-level').click(function (e) {
+    $(this).toggleClass('active');
+    $(this).parent().siblings().find('button').removeClass('active');
+  }); // 選擇結果呈現
+
+  $('.js-firstLvBtn').click(function () {
+    $('.js-firstLv').removeClass('d-none');
+    $('.js-firstLv').siblings().addClass('d-none');
+  });
+  $('.js-midLvBtn').click(function () {
+    $('.js-midLv').removeClass('d-none');
+    $('.js-midLv').siblings().addClass('d-none');
+  });
+  $('.js-highLvBtn').click(function () {
+    $('.js-highLv').removeClass('d-none');
+    $('.js-highLv').siblings().addClass('d-none');
+  }); // vanillajs-datepicker
+
+  var elem = document.querySelector('input[name="datepicker"]');
+  var datepicker = new Datepicker(elem, {
+    buttonClass: 'btn link-primary',
+    nextArrow: '>',
+    prevArrow: '<',
+    todayHighlight: true
+  });
 });
 //# sourceMappingURL=all.js.map
